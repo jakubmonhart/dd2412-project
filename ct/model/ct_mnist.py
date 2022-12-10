86c4ebbac0919f73cd0fb4bfccfc61597a9e45f1
jakubmonhart <monhart.jakub@gmail.com>
2022-12-10 18:03:46 +0000

    mnist: Figure 3 experiment

--- ct/model/ct_mnist.py
+++ ct/model/ct_mnist.py
@@ -39,49 +39,28 @@ class CT_MNIST_torch(nn.Module):
     return out, attn
 
 
-def loss_fn(target_class, target_concept, pred_class, attn, expl_coeff=2.0):
-  
-  pred_class = pred_class.squeeze()
-  target_class = target_class.float()
-  cls_loss = nn.functional.binary_cross_entropy_with_logits(pred_class, target_class)
-  
-  # We are using multiple-head attention -> we need to average over them?
-  attn = attn.squeeze(2)
-  attn = torch.mean(attn, dim=1)
-
-  # TODO - should we normalize the attentions to sum to 1 as they do in the paper?
-  
-  expl_loss = nn.functional.mse_loss(target_concept, attn)
-  
-  loss = cls_loss + expl_coeff*expl_loss
-
-  return loss, cls_loss.item(), expl_loss.item()
-
-
 class CT_MNIST(pl.LightningModule):
-  def __init__(self):
+  def __init__(self, expl_coeff=2.0):
     super().__init__()
     
+    self.expl_coeff = expl_coeff
     self.model = CT_MNIST_torch()
-
-    # self.accuracy = torchmetrics.Accuracy()
-    # self.accuracy_fn = torchmetrics.functional.accuracy
 
     self.train_accuracy = torchmetrics.Accuracy(task='binary')
     self.val_accuracy = torchmetrics.Accuracy(task='binary')
+    self.test_accuracy = torchmetrics.Accuracy(task='binary')
 
   def training_step(self, batch, batch_idx):
     image, (target_class, target_concept) = batch
     pred_class, attn = self.model(image)
 
     # Loss
-    loss, cls_loss, expl_loss = loss_fn(target_class, target_concept, pred_class, attn)
+    loss, cls_loss, expl_loss = self.loss_fn(target_class, target_concept, pred_class, attn)
 
     # Accuracy
     self.train_accuracy(pred_class.squeeze(), target_class.int())
-    # acc = self.accuracy_fn(pred_class, target_class.int(), threshold=0.0)
     self.log('train_cls_loss', cls_loss)
-    self.log('train_expl_loss', expl_loss)
+    self.log('train_expl_loss', expl_loss, prog_bar=True)
     self.log('train_acc', self.train_accuracy, prog_bar=True)
     
     return loss
@@ -91,15 +70,14 @@ class CT_MNIST(pl.LightningModule):
     pred_class, attn = self.model(image)
     
     # Loss
-    loss, cls_loss, expl_loss = loss_fn(target_class, target_concept, pred_class, attn)
+    loss, cls_loss, expl_loss = self.loss_fn(target_class, target_concept, pred_class, attn)
 
     # Accuracy
-    # acc = self.accuracy_fn(pred_class, target_class.int(), threshold=0.0)
     self.val_accuracy(pred_class.squeeze(), target_class.int())
     self.log('val_cls_loss', cls_loss)
-    self.log('val_expl_loss', expl_loss)
+    self.log('val_expl_loss', expl_loss, prog_bar=True)
     self.log('val_acc', self.val_accuracy, prog_bar=True)
-    self.log('val_loss', loss)
+    self.log('val_loss', loss, prog_bar=True)
 
 
   def test_step(self, batch, batch_idx):
@@ -107,14 +85,32 @@ class CT_MNIST(pl.LightningModule):
     pred_class, attn = self.model(image)
     
     # Loss
-    loss, cls_loss, expl_loss = loss_fn(target_class, target_concept, pred_class, attn)
+    loss, cls_loss, expl_loss = self.loss_fn(target_class, target_concept, pred_class, attn)
 
     # Accuracy
-    # acc = self.accuracy_fn(pred_class, target_class.int(), threshold=0.0)
+    self.test_accuracy(pred_class.squeeze(), target_class.int())
     self.log('test_cls_loss', cls_loss)
-    self.log('test_expl_loss', expl_loss)
-    # self.log('test_acc', acc, prog_bar=True)
-    self.log('test_loss', loss)
+    self.log('test_expl_loss', expl_loss, prog_bar=True)
+    self.log('test_acc', self.test_accuracy, prog_bar=True)
+    self.log('test_loss', loss, prog_bar=True)
+
+  def loss_fn(self, target_class, target_concept, pred_class, attn):
+  
+    pred_class = pred_class.squeeze()
+    target_class = target_class.float()
+    cls_loss = nn.functional.binary_cross_entropy_with_logits(pred_class, target_class)
+    
+    # We are using multiple-head attention -> we need to average over them?
+    attn = attn.squeeze(2)
+    attn = torch.mean(attn, dim=1)
+
+    # TODO - should we normalize the attentions to sum to 1 as they do in the paper?
+    
+    expl_loss = nn.functional.mse_loss(target_concept, attn)
+    
+    loss = cls_loss + self.expl_coeff*expl_loss
+
+    return loss, cls_loss.item(), expl_loss.item()
 
   def configure_optimizers(self):
     optimizer = optim.AdamW(self.parameters(), lr=1e-3)
